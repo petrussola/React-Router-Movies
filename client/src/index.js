195fa1562b70a07377baec643299c2ec8ac61335
Pere <pere.solaclaver@gmail.com>
2019-09-16 13:28:18 +0100

    Import browser router and add tags wrapping the app

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRoute } from "react-router-dom";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRoute>
+    <App />
+  </BrowserRoute>,
+  document.getElementById("root")
+);
