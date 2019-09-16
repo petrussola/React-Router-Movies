85507c6c4d09910c5642ef7a619466122f123f15
Pere <pere.solaclaver@gmail.com>
2019-09-16 13:34:00 +0100

    Fix bug where r in BrowserRouter was missing in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRoute as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
