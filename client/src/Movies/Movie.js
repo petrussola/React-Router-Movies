ae22bbc1d4d5e7e45b79ecbade3f3abacbfb918e
Pere <pere.solaclaver@gmail.com>
2019-09-17 13:19:14 +0100

    Fix bug where map function could not run on stars, which was undefined. Reason was because the state was an empty object and there was no stars array to iterate through. Fixed it by either setting state to false or by setting state to {} with stars prop as empty array

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,13 +2,12 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Movie = (props) => {
-  const [movie, setMovie] = useState({});
+  const [movie, setMovie] = useState({stars: []});
  
   useEffect(() => {
     const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
-
        axios
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
@@ -18,8 +17,8 @@ const Movie = (props) => {
           console.error(error);
         });
 
-  },[]);
-  
+  },[props.match.params.id]);
+
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
   //   const addToSavedList = props.addToSavedList;
@@ -42,12 +41,12 @@ const Movie = (props) => {
           Metascore: <strong>{metascore}</strong>
         </div>
         <h3>Actors</h3>
-{console.log(stars)}
-        {/* {stars.map(star => (
+        
+        {stars.map(star => (
           <div key={star} className="movie-star">
             {star}
           </div>
-        ))} */}
+        ))}
       </div>
       <div className="save-button">Save</div>
     </div>
