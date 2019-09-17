db6e004709a57586fdd1010120137e44c266eff3
Pere <pere.solaclaver@gmail.com>
2019-09-17 15:50:44 +0100

    Build functionality so that MOvies saved are links that link to the movie page

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,16 +1,22 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-const SavedList = props => (
-  <div className="saved-list">
-    <h3>Saved Movies:</h3>
-    {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
-    ))}
-    <Link to='/'>
-      <div className="home-button">Home</div>
-    </Link>
-  </div>
-);
+const SavedList = props => {
+  debugger;
+  return (
+    <div className="saved-list">
+      <h3>Saved Movies:</h3>
+      {props.list.map(movie => (
+        // <span className="saved-movie">{movie.title}</span>
+        <Link to={`/movies/${movie.id}`} className="saved-movie">
+          {movie.title}
+        </Link>
+      ))}
+      <Link to="/">
+        <div className="home-button">Home</div>
+      </Link>
+    </div>
+  );
+};
 
 export default SavedList;
